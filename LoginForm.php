da7efa678ad1bd7eae5d27d5c6e32e446702df28
ThangDang1604 <dangvietthang789@gmail.com>
2022-02-22 08:44:49 +0200

    Create LoginForm.php
    Create a login form from Thang Dang

---
+++ LoginForm.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="POST" action="LoginInfo.php">
+        <div class="col-md">
+  	        <label for="fname">first name:</label> <input type="text" id="fname" name="fname" required></p>
+        </div>
+        <div class="col-md">
+  	        <label for="lname">Last name:</label> <input type="text" id="lname" name="lname" required></p>
+        </div>
+        <div class="col-md">
+  	        <label for="email">Email:</label> <input type="text" id="email" name="email" required></p>
+        </div>
+        <div class="col-md">
+            <label for="pass">Password:</label> <input type="text" id="pass" name="pass" required></p>
+        </div>
+        <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+	</form>
+</body>
+<script>
+    const fname = document.getElementById('fname')
+    const lname = document.getElementById('lname')
+    const email = document.getElementById('email')
+    const pass = document.getElementById('pass')
+</script>
+
+</html>
+
